278b76bf101e2c362e8d645f9632b3f1c7de6363
zwx <280940608@qq.com>
2022-02-21 14:09:57 +0800

    2022.2.21

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/paymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/paymentMain8001.java
@@ -3,9 +3,11 @@ package com.atguigu.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.atguigu.springcloud.dao")
+@EnableEurekaClient
 public class paymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(paymentMain8001.class, args);
